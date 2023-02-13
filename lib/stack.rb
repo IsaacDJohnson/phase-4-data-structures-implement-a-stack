cf3672ccb33a2e15f09bd9fd38ca33de59c1e8a1
Isaacjohnson <isaac.designws@gmail.com>
2023-02-13 13:04:37 -0600

    completed

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,19 @@
 # your code here
+class Stack
+
+    def initialize
+        @stack = []
+    end
+
+    def pop
+       @stack.pop 
+    end
+    
+    def peek
+        @stack.last
+    end
+
+    def push(value)
+       @stack.push(value) 
+    end
+end
